1ca10093a77650e053094b5ec219afb7be819de2
Andrew Azores <me@andrewazor.es>
2021-12-21 15:04:13 -0500

    apply spotless formatting

--- src/test/java/io/cryostat/ReportResourceTest.java
+++ src/test/java/io/cryostat/ReportResourceTest.java
@@ -3,10 +3,10 @@ package io.cryostat;
 import static io.restassured.RestAssured.given;
 
 import java.io.File;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
+import io.quarkus.test.junit.QuarkusTest;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.jsoup.Jsoup;
@@ -14,44 +14,39 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.test.junit.QuarkusTest;
-
 @QuarkusTest
 public class ReportResourceTest {
 
     @Test
     public void testHealthEndpoint() {
-        given()
-          .when().get("/health")
-          .then()
-             .statusCode(204);
+        given().when().get("/health").then().statusCode(204);
     }
 
     @Test
     public void testReportEndpoint() throws URISyntaxException {
         File jfr = Paths.get(getClass().getResource("/profiling_sample.jfr").toURI()).toFile();
-        String response = given()
-            .contentType("multipart/form-data")
-            .multiPart("file", jfr)
-            .when()
-            .post("/report")
-            .then()
-            .statusCode(200)
-            .contentType("text/html")
-            .body(Matchers.is(Matchers.not(Matchers.emptyOrNullString())))
-            .extract()
-            .asString();
+        String response =
+                given().contentType("multipart/form-data")
+                        .multiPart("file", jfr)
+                        .when()
+                        .post("/report")
+                        .then()
+                        .statusCode(200)
+                        .contentType("text/html")
+                        .body(Matchers.is(Matchers.not(Matchers.emptyOrNullString())))
+                        .extract()
+                        .asString();
 
-            Document doc = Jsoup.parse(response, "UTF-8");
+        Document doc = Jsoup.parse(response, "UTF-8");
 
-            Elements head = doc.getElementsByTag("head");
-            Elements titles = head.first().getElementsByTag("title");
-            Elements body = doc.getElementsByTag("body");
+        Elements head = doc.getElementsByTag("head");
+        Elements titles = head.first().getElementsByTag("title");
+        Elements body = doc.getElementsByTag("body");
 
-            MatcherAssert.assertThat("Expected one <head>", head.size(), Matchers.equalTo(1));
-            MatcherAssert.assertThat("Expected one <title>", titles.size(), Matchers.equalTo(1));
-            MatcherAssert.assertThat(titles.get(0).html(), Matchers.equalTo("Automated Analysis Result Overview"));
-            MatcherAssert.assertThat("Expected one <body>", body.size(), Matchers.equalTo(1));
+        MatcherAssert.assertThat("Expected one <head>", head.size(), Matchers.equalTo(1));
+        MatcherAssert.assertThat("Expected one <title>", titles.size(), Matchers.equalTo(1));
+        MatcherAssert.assertThat(
+                titles.get(0).html(), Matchers.equalTo("Automated Analysis Result Overview"));
+        MatcherAssert.assertThat("Expected one <body>", body.size(), Matchers.equalTo(1));
     }
-
 }
