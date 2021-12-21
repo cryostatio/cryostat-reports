cb7c4d49f144eb6d4adddecc2b3cba03f8056a73
Andrew Azores <me@andrewazor.es>
2021-12-21 10:39:14 -0500

    test(report): use JSoup to validate HTML report response

--- src/test/java/io/cryostat/ReportResourceTest.java
+++ src/test/java/io/cryostat/ReportResourceTest.java
@@ -3,10 +3,15 @@ package io.cryostat;
 import static io.restassured.RestAssured.given;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
+import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -25,7 +30,7 @@ public class ReportResourceTest {
     @Test
     public void testReportEndpoint() throws URISyntaxException {
         File jfr = Paths.get(getClass().getResource("/profiling_sample.jfr").toURI()).toFile();
-        given()
+        String response = given()
             .contentType("multipart/form-data")
             .multiPart("file", jfr)
             .when()
@@ -34,7 +39,19 @@ public class ReportResourceTest {
             .statusCode(200)
             .contentType("text/html")
             .body(Matchers.is(Matchers.not(Matchers.emptyOrNullString())))
-            ;
+            .extract()
+            .asString();
+
+            Document doc = Jsoup.parse(response, "UTF-8");
+
+            Elements head = doc.getElementsByTag("head");
+            Elements titles = head.first().getElementsByTag("title");
+            Elements body = doc.getElementsByTag("body");
+
+            MatcherAssert.assertThat("Expected one <head>", head.size(), Matchers.equalTo(1));
+            MatcherAssert.assertThat("Expected one <title>", titles.size(), Matchers.equalTo(1));
+            MatcherAssert.assertThat(titles.get(0).html(), Matchers.equalTo("Automated Analysis Result Overview"));
+            MatcherAssert.assertThat("Expected one <body>", body.size(), Matchers.equalTo(1));
     }
 
 }
