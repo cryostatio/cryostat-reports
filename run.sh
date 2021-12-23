1e07b8357a4f1d5a810f32bf702a39024611a0e0
Andrew Azores <me@andrewazor.es>
2021-12-23 11:42:12 -0500

    print command line flags and allow JVM to select own GC algo from ergonomics

--- run.sh
+++ run.sh
@@ -8,10 +8,8 @@ if [ -z "${CPUS}" ]; then
 fi
 
 if [ "${CPUS}" -gt 1 ]; then
-    GC="Parallel"
     SINGLETHREAD_JFR_PARSE="false"
 else
-    GC="Serial"
     SINGLETHREAD_JFR_PARSE="true"
 fi
 
@@ -24,6 +22,6 @@ podman run \
     --cpus "${CPUS}" \
     --memory "${MEMORY}" \
     --publish 8080:8080 \
-    --env JAVA_OPTIONS="-XX:ActiveProcessorCount=${CPUS} -XX:+Use${GC}GC -Dorg.openjdk.jmc.flightrecorder.parser.singlethreaded=${SINGLETHREAD_JFR_PARSE}" \
+    --env JAVA_OPTIONS="-XX:ActiveProcessorCount=${CPUS} -XX:+PrintCommandLineFlags -Dorg.openjdk.jmc.flightrecorder.parser.singlethreaded=${SINGLETHREAD_JFR_PARSE}" \
     --rm -it \
     quay.io/cryostat/cryostat-reports:latest
