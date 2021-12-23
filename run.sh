4792d71e699375a7b45b88f3e53d301b8afe1233
Andrew Azores <me@andrewazor.es>
2021-12-23 12:47:11 -0500

    apply processing timeout and respond 504 if cannot complete in time

--- run.sh
+++ run.sh
@@ -21,11 +21,15 @@ if [ -z "${MEMORY_FACTOR}" ]; then
     MEMORY_FACTOR=10
 fi
 
+if [ -z "${TIMEOUT}" ]; then
+    TIMEOUT=30000
+fi
+
 podman run \
     --user 0 \
     --cpus "${CPUS}" \
     --memory "${MEMORY}" \
     --publish 8080:8080 \
-    --env JAVA_OPTIONS="-XX:ActiveProcessorCount=${CPUS} -XX:+PrintCommandLineFlags -Dorg.openjdk.jmc.flightrecorder.parser.singlethreaded=${SINGLETHREAD_JFR_PARSE} -Dio.cryostat.reports.memory-factor=${MEMORY_FACTOR}" \
+    --env JAVA_OPTIONS="-XX:ActiveProcessorCount=${CPUS} -XX:+PrintCommandLineFlags -Dorg.openjdk.jmc.flightrecorder.parser.singlethreaded=${SINGLETHREAD_JFR_PARSE} -Dio.cryostat.reports.memory-factor=${MEMORY_FACTOR} -Dio.cryostat.reports.timeout=${TIMEOUT}" \
     --rm -it \
     quay.io/cryostat/cryostat-reports:latest
--- src/main/java/io/cryostat/reports/ReportResource.java
+++ src/main/java/io/cryostat/reports/ReportResource.java
@@ -3,16 +3,22 @@ package io.cryostat.reports;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -21,6 +27,7 @@ import io.cryostat.core.sys.FileSystem;
 
 import io.quarkus.runtime.StartupEvent;
 import io.smallrye.common.annotation.Blocking;
+import io.vertx.ext.web.RoutingContext;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.MultipartForm;
@@ -36,17 +43,21 @@ public class ReportResource {
     @ConfigProperty(name = "io.cryostat.reports.memory-factor", defaultValue = "10")
     String memoryFactor;
 
+    @ConfigProperty(name = "io.cryostat.reports.timeout", defaultValue = "29000")
+    String timeoutMs;
+
     @Inject Logger logger;
     @Inject ReportGenerator generator;
     @Inject FileSystem fs;
 
     void onStart(@Observes StartupEvent ev) {
         logger.infof(
-                "CPUs: %d singlethread: %b maxMemory: %dM memoryFactor: %s",
+                "CPUs: %d singlethread: %b maxMemory: %dM memoryFactor: %s timeout: %sms",
                 Runtime.getRuntime().availableProcessors(),
                 Boolean.getBoolean(SINGLETHREAD_PROPERTY),
                 Runtime.getRuntime().maxMemory() / (1024 * 1024),
-                memoryFactor);
+                memoryFactor,
+                timeoutMs);
     }
 
     @Path("health")
@@ -59,19 +70,24 @@ public class ReportResource {
     @POST
     @Produces(MediaType.TEXT_HTML)
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public String addRecording(@MultipartForm RecordingFormData form) throws IOException {
+    public String addRecording(RoutingContext ctx, @MultipartForm RecordingFormData form)
+            throws IOException {
         FileUpload upload = form.file;
         java.nio.file.Path file = upload.uploadedFile();
+        long timeout = TimeUnit.MILLISECONDS.toNanos(Long.parseLong(timeoutMs));
         long start = System.nanoTime();
+        long now = start;
+        long elapsed = 0;
         logger.infof("Received request for %s (%d bytes)", upload.fileName(), upload.size());
 
         if (IOToolkit.isCompressedFile(file.toFile())) {
             file = decompress(file);
+            now = System.nanoTime();
             logger.infof(
                     "%s was compressed. Decompressed size: %d bytes. Decompression took %dms",
                     upload.fileName(),
                     file.toFile().length(),
-                    TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start));
+                    TimeUnit.NANOSECONDS.toMillis(now - start));
         }
 
         Runtime runtime = Runtime.getRuntime();
@@ -82,8 +98,21 @@ public class ReportResource {
             throw new ClientErrorException(Response.Status.REQUEST_ENTITY_TOO_LARGE);
         }
 
+        now = System.nanoTime();
+        elapsed = now - start;
+        if (elapsed > timeout) {
+            throw new ServerErrorException(Response.Status.GATEWAY_TIMEOUT);
+        }
+
+        CompletableFuture<String> result = new CompletableFuture<>();
         try (var stream = fs.newInputStream(file)) {
-            return generator.generateReport(stream);
+            ForkJoinPool.commonPool()
+                    .submit(() -> result.complete(generator.generateReport(stream)));
+            return result.get(timeout - elapsed, TimeUnit.NANOSECONDS);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new InternalServerErrorException(e);
+        } catch (TimeoutException e) {
+            throw new ServerErrorException(Response.Status.GATEWAY_TIMEOUT, e);
         } finally {
             boolean deleted = fs.deleteIfExists(file);
             if (deleted) {
