3b89f1e332f2da17f1caac69207c9c7566eea705
Andrew Azores <me@andrewazor.es>
2022-01-07 10:23:22 -0500

    test(report): add compressed recording unit test

--- src/test/java/io/cryostat/ReportResourceTest.java
+++ src/test/java/io/cryostat/ReportResourceTest.java
@@ -49,4 +49,32 @@ public class ReportResourceTest {
                 titles.get(0).html(), Matchers.equalTo("Automated Analysis Result Overview"));
         MatcherAssert.assertThat("Expected one <body>", body.size(), Matchers.equalTo(1));
     }
+
+    @Test
+    public void testReportEndpointWithCompressedFile() throws URISyntaxException {
+        File jfr = Paths.get(getClass().getResource("/profiling_sample.jfr.gz").toURI()).toFile();
+        String response =
+                given().contentType("multipart/form-data")
+                        .multiPart("file", jfr)
+                        .when()
+                        .post("/report")
+                        .then()
+                        .statusCode(200)
+                        .contentType("text/html")
+                        .body(Matchers.is(Matchers.not(Matchers.emptyOrNullString())))
+                        .extract()
+                        .asString();
+
+        Document doc = Jsoup.parse(response, "UTF-8");
+
+        Elements head = doc.getElementsByTag("head");
+        Elements titles = head.first().getElementsByTag("title");
+        Elements body = doc.getElementsByTag("body");
+
+        MatcherAssert.assertThat("Expected one <head>", head.size(), Matchers.equalTo(1));
+        MatcherAssert.assertThat("Expected one <title>", titles.size(), Matchers.equalTo(1));
+        MatcherAssert.assertThat(
+                titles.get(0).html(), Matchers.equalTo("Automated Analysis Result Overview"));
+        MatcherAssert.assertThat("Expected one <body>", body.size(), Matchers.equalTo(1));
+    }
 }
