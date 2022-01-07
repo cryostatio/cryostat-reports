687123972c5597294f712c85908fb0e3391dc691
Andrew Azores <me@andrewazor.es>
2022-01-07 10:26:48 -0500

    refactor(reporttest): use parameterized test

--- src/test/java/io/cryostat/ReportResourceTest.java
+++ src/test/java/io/cryostat/ReportResourceTest.java
@@ -13,6 +13,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @QuarkusTest
 public class ReportResourceTest {
@@ -22,37 +24,14 @@ public class ReportResourceTest {
         given().when().get("/health").then().statusCode(204);
     }
 
-    @Test
-    public void testReportEndpoint() throws URISyntaxException {
-        File jfr = Paths.get(getClass().getResource("/profiling_sample.jfr").toURI()).toFile();
-        String response =
-                given().contentType("multipart/form-data")
-                        .multiPart("file", jfr)
-                        .when()
-                        .post("/report")
-                        .then()
-                        .statusCode(200)
-                        .contentType("text/html")
-                        .body(Matchers.is(Matchers.not(Matchers.emptyOrNullString())))
-                        .extract()
-                        .asString();
-
-        Document doc = Jsoup.parse(response, "UTF-8");
-
-        Elements head = doc.getElementsByTag("head");
-        Elements titles = head.first().getElementsByTag("title");
-        Elements body = doc.getElementsByTag("body");
-
-        MatcherAssert.assertThat("Expected one <head>", head.size(), Matchers.equalTo(1));
-        MatcherAssert.assertThat("Expected one <title>", titles.size(), Matchers.equalTo(1));
-        MatcherAssert.assertThat(
-                titles.get(0).html(), Matchers.equalTo("Automated Analysis Result Overview"));
-        MatcherAssert.assertThat("Expected one <body>", body.size(), Matchers.equalTo(1));
-    }
-
-    @Test
-    public void testReportEndpointWithCompressedFile() throws URISyntaxException {
-        File jfr = Paths.get(getClass().getResource("/profiling_sample.jfr.gz").toURI()).toFile();
+    @ParameterizedTest
+    @ValueSource(
+            strings = {
+                "/profiling_sample.jfr",
+                "/profiling_sample.jfr.gz",
+            })
+    public void testReportEndpoint(String filePath) throws URISyntaxException {
+        File jfr = Paths.get(getClass().getResource(filePath).toURI()).toFile();
         String response =
                 given().contentType("multipart/form-data")
                         .multiPart("file", jfr)
