3dbaa17b79b1a54fdefc628b9f497164b81dd706
Andrew Azores <aazores@redhat.com>
2024-01-24 16:50:15 -0500

    feat(s3): add endpoint for requesting report generation from presigned S3 URL

--- src/main/java/io/cryostat/reports/ReportResource.java
+++ src/main/java/io/cryostat/reports/ReportResource.java
@@ -15,10 +15,17 @@
  */
 package io.cryostat.reports;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.channels.Channels;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.time.Duration;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -47,6 +54,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.ServerErrorException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
 import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
@@ -66,9 +74,19 @@ public class ReportResource {
     @ConfigProperty(name = "io.cryostat.reports.timeout", defaultValue = "29000")
     String timeoutMs;
 
-    @Inject Logger logger;
+    @ConfigProperty(name = "cryostat.storage.base-uri")
+    Optional<String> storageBase;
+
+    @ConfigProperty(name = "cryostat.storage.auth-method")
+    Optional<String> storageAuthMethod;
+
+    @ConfigProperty(name = "cryostat.storage.auth")
+    Optional<String> storageAuth;
+
     @Inject InterruptibleReportGenerator generator;
     @Inject FileSystem fs;
+    @Inject ObjectMapper mapper;
+    @Inject Logger logger;
 
     RuleFilterParser rfp = new RuleFilterParser();
 
@@ -88,11 +106,70 @@ public class ReportResource {
     public void healthCheck() {}
 
     @Blocking
+    @Path("remote_report")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @POST
+    public String getReportFromPresigned(RoutingContext ctx, @BeanParam PresignedFormData form)
+            throws IOException, URISyntaxException {
+        try {
+            long timeout = TimeUnit.MILLISECONDS.toNanos(Long.parseLong(timeoutMs));
+            long start = System.nanoTime();
+
+            var file = Files.createTempFile(null, null);
+
+            UriBuilder uriBuilder =
+                    UriBuilder.newInstance()
+                            .uri(new URI(storageBase.get()))
+                            .path(form.path)
+                            .replaceQuery(form.query);
+            URI downloadUri = uriBuilder.build();
+            logger.infov("Attempting to download presigned recording from {0}", downloadUri);
+            HttpURLConnection httpConn = (HttpURLConnection) downloadUri.toURL().openConnection();
+            httpConn.setRequestMethod("GET");
+            if (storageAuth.isPresent() && storageAuth.isPresent()) {
+                httpConn.setRequestProperty(
+                        "Authorization",
+                        String.format("%s %s", storageAuthMethod.get(), storageAuth.get()));
+            }
+            try (var stream = httpConn.getInputStream();
+                    var downloadChannel = Channels.newChannel(stream);
+                    var fos = new FileOutputStream(file.toFile());
+                    var fileChannel = fos.getChannel(); ) {
+                fileChannel.transferFrom(downloadChannel, 0, Long.MAX_VALUE);
+            } finally {
+                httpConn.disconnect();
+            }
+            long elapsed = System.nanoTime() - start;
+            logger.infov("Downloaded {0} in {1}", downloadUri, Duration.ofNanos(elapsed));
+
+            Predicate<IRule> predicate = rfp.parse(form.filter);
+            Future<Map<String, AnalysisResult>> evalMapFuture = null;
+
+            try (var stream = fs.newInputStream(file)) {
+                evalMapFuture = generator.generateEvalMapInterruptibly(stream, predicate);
+                ctxHelper(ctx, evalMapFuture);
+                return mapper.writeValueAsString(
+                        evalMapFuture.get(timeout - elapsed, TimeUnit.NANOSECONDS));
+            } catch (ExecutionException | InterruptedException e) {
+                throw new InternalServerErrorException(e);
+            } catch (TimeoutException e) {
+                throw new ServerErrorException(Response.Status.GATEWAY_TIMEOUT, e);
+            } finally {
+                cleanupHelper(evalMapFuture, file, file.toFile().getName(), start);
+            }
+        } catch (Exception e) {
+            logger.error(e);
+            throw e;
+        }
+    }
+
+    @Blocking
     @Path("report")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @POST
-    public String getEval(RoutingContext ctx, @BeanParam RecordingFormData form)
+    public String getReport(RoutingContext ctx, @BeanParam RecordingFormData form)
             throws IOException {
         FileUpload upload = form.file;
 
@@ -105,11 +182,10 @@ public class ReportResource {
         Predicate<IRule> predicate = rfp.parse(form.filter);
         Future<Map<String, AnalysisResult>> evalMapFuture = null;
 
-        ObjectMapper oMapper = new ObjectMapper();
         try (var stream = fs.newInputStream(file)) {
             evalMapFuture = generator.generateEvalMapInterruptibly(stream, predicate);
             ctxHelper(ctx, evalMapFuture);
-            return oMapper.writeValueAsString(
+            return mapper.writeValueAsString(
                     evalMapFuture.get(timeout - elapsed, TimeUnit.NANOSECONDS));
         } catch (ExecutionException | InterruptedException e) {
             throw new InternalServerErrorException(e);
